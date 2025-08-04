4b5d56546a33f1c9030875da0b4d015dc9d8da5e
DevWithToby <devwithtoby@gmail.com>
2025-08-04 13:02:45 +0530

    Added getEmployeByID and a custom exception

--- src/main/java/com/app/ems/controller/EmployeeController.java
+++ src/main/java/com/app/ems/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.app.ems.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -24,4 +21,9 @@ public class EmployeeController {
         return new ResponseEntity<>(savedEmployeeDto, HttpStatus.CREATED);
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
 }
